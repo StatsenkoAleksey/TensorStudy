97e825d98fd9c134a7232103e648ba7f2e81feba
StatsenkoAleksey <64027378+StatsenkoAleksey@users.noreply.github.com>
2020-05-31 18:16:53 +0300

    Add files via upload

--- public/test/test.js
+++ public/test/test.js
@@ -55,6 +55,95 @@ describe("Тестирование Фабрики", function() {
         const person = personFactory.create(params);
         //assert
         assert(person instanceof Teacher);
+    });
+
+    
+    it('Тест функции возврата даты рождения', function() {
+        // arrange
+        const params = {
+            fullName: "Вика Цукерберг",
+            photo: "img/ava05.jpg",
+            university: "БГПУ",
+            post: "Преподаватель",
+            birthDate: "2001-02-08T00:00:00.000Z",
+            phone: "+7 (963) 123-45-67",
+            active: "2020-03-05T15:00:00.000Z"
+        };
+
+        // act
+        const personFactory = new PersonFactory(params);
+        const person = personFactory.create(params);
+
+        const birthdayStr = person.birthDateStr;
+
+        //assert
+        assert.equal(birthdayStr, '8 февраля');
+    });
+
+    
+    it('Тест функции возврата возраста "год"', function() {
+        // arrange
+        const params = {
+            fullName: "Вика Цукерберг",
+            photo: "img/ava05.jpg",
+            university: "БГПУ",
+            post: "Преподаватель",
+            birthDate: "1999-02-08T00:00:00.000Z",
+            phone: "+7 (963) 123-45-67",
+            active: "2020-03-05T15:00:00.000Z"
+        };
+
+        // act
+        const personFactory = new PersonFactory(params);
+        const person = personFactory.create(params);
+        const birthdayStr = person.age;
+
+        //assert
+        assert.equal(birthdayStr, '21 год');
+    });
+
+    
+    it('Тест функции возврата возраста "года"', function() {
+        // arrange
+        const params = {
+            fullName: "Вика Цукерберг",
+            photo: "img/ava05.jpg",
+            university: "БГПУ",
+            post: "Преподаватель",
+            birthDate: "1998-02-08T00:00:00.000Z",
+            phone: "+7 (963) 123-45-67",
+            active: "2020-03-05T15:00:00.000Z"
+        };
+
+        // act
+        const personFactory = new PersonFactory(params);
+        const person = personFactory.create(params);
+        const birthdayStr = person.age;
+
+        //assert
+        assert.equal(birthdayStr, '22 года');
+    });
+
+    
+    it('Тест функции возврата возраста "лет"', function() {
+        // arrange
+        const params = {
+            fullName: "Вика Цукерберг",
+            photo: "img/ava05.jpg",
+            university: "БГПУ",
+            post: "Преподаватель",
+            birthDate: "2001-02-08T00:00:00.000Z",
+            phone: "+7 (963) 123-45-67",
+            active: "2020-03-05T15:00:00.000Z"
+        };
+
+        // act
+        const personFactory = new PersonFactory(params);
+        const person = personFactory.create(params);
+        const birthdayStr = person.age;
+
+        //assert
+        assert.equal(birthdayStr, '19 лет');
     })
 });
 
